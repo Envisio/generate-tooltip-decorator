c99bf9ef0649cddc6fc6394b5eeae8aa904704a6
Barry Shao <bshao@envisio.com>
2018-06-14 15:21:12 -0700

    for Highcharts, remember there still with something need to do after.

---
+++ src/index.js
@@ -0,0 +1,17 @@
+export default function ({ shared, chartType }) {
+  // TODO: Boys, enabled shared tooltip by calling generateTooltipDecorator(true)
+  // and set `shared: true` on `tooltip` at the caller level
+  if (shared !== false) {
+    throw new Error('generateTooltipDecorator does not support shared tooltip yet.');
+  }
+  return function generateTooltipDecorator() {
+    const {
+      series: { data, options: { displayData } }, percentage, key, x, point,
+    } = this;
+    const index = data.indexOf(point);
+    if (chartType === 'pie') {
+      return `${key}<br />${displayData[index]} - ${percentage.toFixed(2)}%`;
+    }
+    return `${x}<br />${displayData[index]}`;
+  };
+}
