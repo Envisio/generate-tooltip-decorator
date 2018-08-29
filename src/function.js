b7c0b653f17d1935328191514792d9a36ebe454a
Derrell Wong <dwong@envisio.com>
2018-08-29 09:18:06 -0700

    Change export code and update package to v10

--- src/function.js
+++ src/function.js
@@ -1,9 +1,11 @@
-var Envisio = Envisio || {}; // eslint-disable-line no-use-before-define
 
-Envisio.generateTooltipDecorator = Envisio.generateTooltipDecorator || function (_ref) {
+const Envisio = Envisio || {}; // eslint-disable-line no-use-before-define
+Envisio.generateTooltipDecorator = Envisio.generateTooltipDecorator
+  || function ({ shared, chartType }) {
   if (shared !== false) {
     throw new Error('generateTooltipDecorator does not support shared tooltip yet.');
   }
+
   return function generateTooltipDecorator() {
     const {
       series: { data, options: { displayData = [], toolTipComments = [] } }, percentage, key, x, point,
@@ -28,9 +30,8 @@ Envisio.generateTooltipDecorator = Envisio.generateTooltipDecorator || function 
     if (toolTipComments[index].includes('rc-widget-tooltip-comment') && toolTipComments[index].includes('<br />')) {
       comment = toolTipComments[index].replace(/<br ?\/?>/g, '');
       const regex = /<span class="rc-widget-tooltip-comment">(.*)<\/span>/;
-      comment = comment
-        .replace(regex, '$1')
-        .match(/([^\s]*\s[^\s]*){0,20}/g).join(' <br /> ');
+      comment = comment.replace(regex, '$1')
+      comment = comment.match(/([^\s]*\s[^\s]*){0,20}/g).join(' <br /> ');
       return `<span>${x}<br />${displayData[index]}<br />${comment}</span>`;
     }
     comment = toolTipComments[index].match(/([^\s]*\s[^\s]*){0,20}/g).join(' <br /> ');
